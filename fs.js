03d9d3825dea8b250099efd4d90e36f4a28eefcd
Lemon <2514516335@qq.com>
2025-02-15 22:27:07 +0800

    Initial commit

---
+++ fs.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+// 函数：查看对应路径的文件或者文件夹的大小
+function getSize(path) {
+    const stats = fs.statSync(path);
+    if (stats.isFile()) {
+        return stats.size;
+    } else if (stats.isDirectory()) {
+        let totalSize = 0;
+        const files = fs.readdirSync(path);
+        files.forEach(file => {
+            totalSize += getSize(`${path}/${file}`);
+        });
+        return totalSize;
+    }
+}
+
+const targetPath = path.resolve(__dirname, './fs.js');
+console.log("targetpath:", targetPath);
+let size = getSize(targetPath);
+
+console.log(size);
